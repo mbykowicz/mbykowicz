aa6c894d084454e7b515a55e95b99b68e18bf24f
Mateusz Bykowicz <mbykowicz@gmail.com>
2024-01-16 19:41:44 +0100

    add responsive behavior to navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 import avatarImage from '../public/avatar.png'
@@ -8,6 +8,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import HomeIcon from './icons/HomeIcon'
 import FileDownloadIcon from './icons/FileDownloadIcon'
+import MenuIcon from './icons/MenuIcon'
 
 const items = [
   {
@@ -31,23 +32,29 @@ const items = [
 function NavBar() {
   const pathname = usePathname()
 
+  const [isOpen, setIsOpen] = useState(true)
+
   return (
     <header className='flex items-center justify-between w-full h-[var(--navbar-height)] mt-[var(--navbar-top-margin)]'>
       <Link href='/'>
-        <div className='overflow-hidden border rounded-full shadow-md shadow-gray-100 w-14 h-14 ring-4 ring-gray-100'>
+        <div className='overflow-hidden border rounded-full w-14 h-14 ring-4 ring-gray-100'>
           <Image src={avatarImage} alt='Picture of myself' />
         </div>
       </Link>
 
-      <nav>
-        <ul className='flex items-center h-12 gap-10 px-12 border rounded-full shadow-md shadow-gray-100'>
+      <nav
+        className={`absolute inset-y-0 right-0 z-40 w-2/3 px-12 py-12 bg-white border shadow-md space-y-12 lg:static lg:py-0 lg:w-auto lg:rounded-full shadow-gray-100 transition-all ${
+          isOpen ? 'translate-x-0' : 'translate-x-full'
+        }`}
+      >
+        <ul className='flex flex-col items-center gap-10 lg:flex-row lg:h-12'>
           {items.map((item, index) => (
-            <li key={index}>
+            <li key={index} className='w-full lg:w-auto'>
               <Link
                 href={item.href}
-                className={`text-sm font-medium py-3 px-2 border-b-2 transition ${
+                className={`text-sm font-medium text-center py-3 px-2 border-b-2 transition block ${
                   pathname === item.href
-                    ? 'text-gray-900  border-mountain-meadow-600'
+                    ? 'text-gray-900 border-transparent lg:border-mountain-meadow-600'
                     : 'text-gray-600 border-transparent'
                 }`}
               >
@@ -56,8 +63,21 @@ function NavBar() {
             </li>
           ))}
         </ul>
+        <ul className='border-t lg:hidden'>
+          <li>
+            <Link
+              href='/'
+              className='flex items-center justify-center gap-2 py-10'
+            >
+              <FileDownloadIcon className='w-4 h-4 text-mountain-meadow-600' />
+              <span className='text-sm font-medium text-gray-600'>
+                Download CV
+              </span>
+            </Link>
+          </li>
+        </ul>
       </nav>
-      <Link href='/'>
+      <Link href='/' className='hidden lg:block'>
         <div className='flex items-center justify-center h-10 gap-1 px-6 overflow-hidden border rounded-full shadow-md shadow-gray-100 ring-4 ring-gray-100'>
           <FileDownloadIcon className='w-4 h-4 text-mountain-meadow-600' />
           <span className='text-[11px] font-semibold text-gray-600'>
@@ -65,6 +85,12 @@ function NavBar() {
           </span>
         </div>
       </Link>
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className='relative z-50 grid border border-gray-200 rounded-full place-content-center ring-4 ring-gray-100 w-14 h-14 lg:hidden'
+      >
+        <MenuIcon className='w-6 h-6 text-gray-600' />
+      </button>
     </header>
   )
 }
